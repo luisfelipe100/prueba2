1480f47877ac504a50d7113bfc90675f1a4bb302
Luis Felipe <actuario.felipe@gmail.com>
2017-04-18 21:35:13 -0500

    Tercer commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,6 @@
 class Inventory < ApplicationRecord
   belongs_to :category
+  validates :wheel_size, numericality: {
+    greater_than: 0
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419011838) do
+ActiveRecord::Schema.define(version: 20170419020608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170419011838) do
     t.datetime "updated_at",    null: false
     t.index ["category_id"], name: "index_inventories_on_category_id", using: :btree
     t.index ["serial_number"], name: "index_inventories_on_serial_number", unique: true, using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
