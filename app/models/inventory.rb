458920e42fb6001f9e399f580779a1e6ac4c3ab6
Luis Felipe <actuario.felipe@gmail.com>
2017-04-19 16:11:41 -0500

    Septimo commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -4,4 +4,6 @@ class Inventory < ApplicationRecord
   validates :wheel_size, numericality: {
     greater_than: 0
   }
+
+  scope :wheel_size_x, ->(x) {where('wheel_size = ?', x)}
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,8 @@
 class Work < ApplicationRecord
   belongs_to :user
   has_one :inventory
+
+  scope :ultimos5, -> { order('id desc').limit(5) }
+  scope :null_item, -> { joins("LEFT JOIN inventories ON inventories.work_id = works.id").where('inventories.work_id is null')}
+
 end
